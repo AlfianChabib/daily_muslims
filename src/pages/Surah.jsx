6d733885cfa88321e2bb850ab8444efdbc6e3e43
Alfian Chabib <alfianchabib109@gmail.com>
2023-10-09 20:29:21 +0700

    buat ui surah detail

--- src/pages/Surah.jsx
+++ src/pages/Surah.jsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useMessage, useSurah } from "../stores/Surah";
 import { getOneSurah } from "../utils/getDataQuran";
 import NavbarTop from "../components/templates/Navbar/NavbarTop";
+import HeaderSurah from "../components/templates/Surah/HeaderSurah";
+import AyatCard from "../components/templates/Surah/AyatCard";
 
 export default function Surah() {
   const { id } = useParams();
   const { oneSurah, setOneSurah } = useSurah();
   const { message, setMessage } = useMessage();
+  const [displayArti, setDisplayArti] = useState(false);
+  const ayat = oneSurah?.ayat;
 
   useEffect(() => {
     getOneSurah(id)
@@ -22,31 +26,23 @@ export default function Surah() {
       .catch((err) => console.log(err));
   }, [id, setMessage, setOneSurah]);
 
-  console.log(oneSurah);
+  // console.log(oneSurah?.ayat);
+  console.log(displayArti);
 
   // Catatan: udah berhasil dapetin data per surat nya, trus udah berhasil nanganin klo mislkn id yang dicari itu gaada. Tinggal dipke aja datanya
 
   return (
     <section>
       <NavbarTop text={"Surah " + oneSurah?.namaLatin} />
-      <div className="flex flex-col w-full px-4 pt-4">
-        <div className="flex bg-[#397D54] py-[10px] px-3 rounded-xl justify-between">
-          <div className="flex flex-col text-white">
-            <h2 className="text-2xl font-semibold mb-4">
-              {oneSurah?.namaLatin}
-            </h2>
-            <div>
-              <p className="font-normal text-sm">{oneSurah?.arti}</p>
-              <p className="text-base">
-                {oneSurah?.tempatTurun} <span>|</span> {oneSurah?.jumlahAyat}{" "}
-                Ayat
-              </p>
-            </div>
-          </div>
-          <div className="text-white text-2xl arabic">
-            <p>{oneSurah?.nama}</p>
-          </div>
-        </div>
+      <div className="flex flex-col w-full px-4 pt-4 mb-12">
+        <HeaderSurah
+          data={oneSurah}
+          setDisplayArti={setDisplayArti}
+          displayArti={displayArti}
+        />
+        {ayat?.map((item, index) => {
+          return <AyatCard key={index} data={item} displayArti={displayArti} />;
+        })}
       </div>
     </section>
   );
