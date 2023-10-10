ba12dafc16a6ccd5d6c131be30cdc3fc21e8cada
Alfian Chabib <alfianchabib109@gmail.com>
2023-10-10 12:53:40 +0700

    buat fitur play audio

--- src/components/templates/Surah/AyatCard.jsx
+++ src/components/templates/Surah/AyatCard.jsx
@@ -7,11 +7,13 @@ AyatCard.propTypes = {
 };
 
 export default function AyatCard({ data, displayArti }) {
+  const audio = data?.audio["03"];
+
   return (
     <div className="flex flex-col mt-3 bg-white drop-shadow-dm1 rounded-md overflow-hidden">
       <div className="flex justify-between py-[6px] px-3 bg-gray-200">
         <p className="text-[#397D54] font-medium text-sm">{data?.nomorAyat}</p>
-        <CardIcon />
+        <CardIcon audio={audio} />
       </div>
       <div className="flex flex-col justify-between gap-4 py-[10px] px-3 text-[#397D54]">
         <p className="text-end text-xl leading-10 arabic">{data?.teksArab}</p>
