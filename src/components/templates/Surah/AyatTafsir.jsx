7684e6720933ab5bf43e1313d2742fb7dbc34ad6
Alfian Chabib <alfianchabib109@gmail.com>
2023-10-18 15:07:29 +0700

    done surah page

---
+++ src/components/templates/Surah/AyatTafsir.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+
+AyatTafsir.propTypes = {
+  tafsir: PropTypes.array,
+  ayat: PropTypes.number,
+  setDisplayTafsir: PropTypes.func,
+};
+
+export default function AyatTafsir(props) {
+  const { tafsir, ayat, setDisplayTafsir } = props;
+
+  return (
+    <div className="flex flex-col drop-shadow-2xl mt-2">
+      <div className="rounded-lg border overflow-hidden">
+        <div className="flex items-center justify-between text-white py-[6px] px-3 bg-[#397D54]">
+          <p>Tafsir {ayat}</p>
+          <button
+            className="text-[#397D54] bg-white w-7 h-7 text-center leading-none font-medium text-xl rounded-full"
+            onClick={() => setDisplayTafsir(false)}
+          >
+            ×
+          </button>
+        </div>
+        <div className="h-[30rem] bg-white py-[6px] px-3 overflow-y-scroll overflow-hidden">
+          <p
+            className="text-[#397D54] font-medium text-sm"
+            dangerouslySetInnerHTML={{ __html: tafsir[ayat - 1].teks }}
+          ></p>
+        </div>
+      </div>
+    </div>
+  );
+}
